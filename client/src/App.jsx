4b753d231fc2cc9e0f9d9b4ba7ecd6411b2f4e12
ElijahWagner007 <elijahnwagner@gmail.com>
2024-02-12 21:43:01 +0800

    testing still

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import { Box, Center } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 
 function App() {
   return (
-    <Box bg="black" color='white' textAlign={'center'}>
+    <Box bg="black" color='white' textAlign={'left'} pl={4}>
       Hello World
     </Box>
   )
