cbea26ef994bd413e4a4f9915f1669cfecec5801
ElijahWagner007 <elijahnwagner@gmail.com>
2024-03-22 15:46:46 -0400

    Adding splash screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const { initialSignIn, signedOut } = require("./signin");
+const { spawn } = require("child_process");
 
 let mainWindow;
 
@@ -7,12 +8,24 @@ function createWindow() {
 	mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
+		show: false,
 		icon: __dirname + "/assets/onshape.png",
 		webPreferences: {
 			nodeIntegration: true,
 			contextIsolation: false,
 		},
 	});
+
+	var splash =  new BrowserWindow({
+		width: 500,
+		height: 300,
+		transparent: true,
+		frame: false,
+		alwaysOnTop: true
+	})
+
+	splash.loadFile('src/splash/splash.html')
+	splash.center()
 
 	mainWindow.loadURL("https://cad.onshape.com/");
 
@@ -35,6 +48,10 @@ function createWindow() {
 
 	mainWindow.webContents.on("did-navigate", handleNavigation);
 	mainWindow.webContents.on("did-navigate-in-page", handleNavigation);
+	setTimeout(function () {
+		splash.close()
+		mainWindow.show()
+	}, 5000);
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then((createWindow));
